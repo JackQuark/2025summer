ee38d0745d60cdb3992eab29c0c9ebe716f5a178
Quark <yen169054@gmail.com>
2025-07-03 16:36:10 +0800

    complete qv pert LRF func in LRF_output.py | rewrite climate val calc. method

--- prec/LRF/LRF_output.py
+++ prec/LRF/LRF_output.py
@@ -14,8 +14,66 @@ zonal_T  = np.load("/home/Quark/2025summer/prec/dycore/meanT.npy")
 zonal_qv = np.load("/home/Quark/2025summer/prec/dycore/meanqv.npy")
 zonal_p  = np.load("/home/Quark/2025summer/prec/dycore/meanp.npy") / 100.
 
+def LRF_calc_qv(p, t, qv, t_surf=288.):
+    nlev = len(p)
+    qvs  = climlab.utils.thermo.qsat(t, p)
+    
+    state = climlab.column_state(lev=p, water_depth=1.0)
+    state['Tatm'][:] = t
+    state['Ts'][:] = t_surf
+    
+    rad_base = climlab.radiation.RRTMG(
+        name='Rad_base',
+        state=state,
+        specific_humidity=qvs,
+        albedo=0.3
+        )
 
-def LRF_calc(p, t, qv, t_surf=288., ofname='LRF_kernel.png'):
+    rad_base.compute_diagnostics()
+    LW_ref = rad_base.diagnostics['TdotLW'].copy()
+    SW_ref = rad_base.diagnostics['TdotSW'].copy()
+
+    kernel_LW = np.zeros((nlev, nlev))
+    kernel_SW = np.zeros((nlev, nlev))
+      
+    for k in range(nlev):
+        # qv perturbation at k lev (-20%RH)
+        tmp = qvs.copy()
+        deltaqv = qvs[k] * -0.2
+        tmp[k] += deltaqv
+
+        # Fresh radiation model per perturbation
+        rad_pert = climlab.radiation.RRTMG(name=f'Rad_pert_{k}',
+                                        state=state,
+                                        specific_humidity=tmp,
+                                        albedo=0.3)
+        rad_pert.compute_diagnostics()
+        LW_pert = rad_pert.diagnostics['TdotLW'].copy()
+        SW_pert = rad_pert.diagnostics['TdotSW'].copy()
+
+        # Compute kernel column (response at all levels to impulse at level k)
+        kernel_LW[:, k] = (LW_pert - LW_ref) # / deltaqv
+        kernel_SW[:, k] = (SW_pert - SW_ref) # / deltaqv
+    
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5), sharex=True, sharey=True)
+    cs1 = axs[0].pcolormesh(p, p, (kernel_LW), cmap='RdBu_r',
+                            vmin=-0.5, vmax=0.5)
+    cs2 = axs[1].pcolormesh(p, p, (kernel_SW), cmap='RdBu_r',
+                            vmin=-0.5, vmax=0.5)
+    cbar = fig.colorbar(cs1, ax=axs)
+    for ax in axs:
+        ax.set_aspect('equal')
+    del ax
+    axs[0].set_xlim([1000, 10])
+    axs[0].set_ylim([1000, 10])
+    axs[0].set_xlabel('Perturbation level [hPa]')
+    axs[0].set_ylabel('Response level [hPa]')
+    axs[0].set_title('R_LW from qv [K/day per -20%RH]')
+    axs[1].set_title('R_SW from qv [K/day per -20%RH]')
+    
+    return kernel_LW, kernel_SW
+
+def LRF_calc_T(p, t, qv, t_surf=288., ofname='LRF_kernel.png'):
     nlev = len(p)
     qvs  = climlab.utils.thermo.qsat(t, p)
     
@@ -60,30 +118,42 @@ def LRF_calc(p, t, qv, t_surf=288., ofname='LRF_kernel.png'):
         kernel_LW[:, k] = (LW_pert - LW_ref) / delta_T
         kernel_SW[:, k] = (SW_pert - SW_ref) / delta_T
 
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5), sharex=True, sharey=True)
+    cs1 = axs[0].pcolormesh(p, p, (kernel_LW), cmap='RdBu_r',
+                            vmin=-0.5, vmax=0.5)
+    cs2 = axs[1].pcolormesh(p, p, (kernel_SW), cmap='RdBu_r',
+                            vmin=-0.5, vmax=0.5)
+    cbar = fig.colorbar(cs1, ax=axs)
+    for ax in axs:
+        ax.set_aspect('equal')
+    del ax
+    axs[0].set_xlim([1000, 10])
+    axs[0].set_ylim([1000, 10])
+    axs[0].set_xlabel('Perturbation level [hPa]')
+    axs[0].set_ylabel('Response level [hPa]')
+    axs[0].set_title('R_LW from T [K/day per K]')
+    axs[1].set_title('R_SW from T [K/day per K]')
+    
     return kernel_LW, kernel_SW
-        
-    # fig, axs = plt.subplots(1, 2, figsize=(10, 5), sharex=True, sharey=True)
-    # cs1 = axs[0].pcolormesh(p, p, (kernel_LW), cmap='RdBu_r',
-    #                         vmin=-0.5, vmax=0.5)
-    # cs2 = axs[1].pcolormesh(p, p, (kernel_SW), cmap='RdBu_r',
-    #                         vmin=-0.5, vmax=0.5)
-    # cbar = fig.colorbar(cs1, ax=axs)
-    # for ax in axs:
-    #     ax.set_aspect('equal')
-    # del ax
-    # axs[0].set_xlim([1000, 100])
-    # axs[0].set_ylim([1000, 100])
-    # axs[0].set_xlabel('Perturbation level [hPa]')
-    # axs[0].set_ylabel('Response level [hPa]')
-    # axs[0].set_title('R_LW from qv [K/day per K]')
-    # axs[1].set_title('R_SW from qv [K/day per K]')
     # fig.savefig(ofname, dpi=150, bbox_inches='tight')
 
 def main():
     total_LRF = np.zeros((64, 20, 20))
-
-    for ilat in range(32, 64):
-        LW, SW =  LRF_calc(
+    nlat, nlon = (64, 128)
+    dlat = 180.0 / nlat 
+    lat_centers = -90 + dlat/2 + np.arange(nlat) * dlat
+    
+    
+    for ilat in range(32, 64, 8):
+        LW, SW =  LRF_calc_qv(
+            zonal_p[:, ilat], 
+            zonal_T[:, ilat], 
+            zonal_qv[:, ilat]
+            )
+        total_LRF[ilat, ...] = LW + SW
+        
+        continue
+        LW, SW =  LRF_calc_T(
             zonal_p[:, ilat], 
             zonal_T[:, ilat], 
             zonal_qv[:, ilat],
@@ -91,8 +161,8 @@ def main():
             )        
         total_LRF[ilat, ...] = LW + SW
     
-    total_LRF[:32, ...] = np.flip(total_LRF[32:, ...], axis=0)
-    np.save('/home/Quark/2025summer/prec/LRF/LRF_Tpert.npy', total_LRF)
+    # total_LRF[:32, ...] = np.flip(total_LRF[32:, ...], axis=0)
+    # np.save('/home/Quark/2025summer/prec/LRF/LRF_Tpert.npy', total_LRF)
 
 # ==================================================
 from time import perf_counter
--- prec/dycore/climate_calc.py
+++ prec/dycore/climate_calc.py
@@ -12,7 +12,7 @@ from dycore_reader import Dataset
 # calc. the mean state of atm from 1000 ~ 2000d data
 
 def save_mean_state(ds: Dataset):
-    f = ds.open_HDF5(1000)    
+    f = ds.open_HDF5(1000)
     meanT_tot = np.zeros((20, 64))
     meanqv_tot = np.zeros((20, 64))
     count = 0
@@ -32,7 +32,7 @@ def save_mean_state(ds: Dataset):
     np.save("meanqv.npy", meanqv_tot)
     
     
-def main():
+def test():
     datadir = "/data92/Quark/ctrl_2000d/HSt42_20"
     ds = Dataset(datadir)
     f = ds.open_HDF5(1000)
@@ -53,6 +53,42 @@ def main():
     
     meanp_tot /= count
     # np.save("meanp.npy", meanp_tot)
+
+def interp_to_ref(x, xp, fp):
+    # data points (xp, fp), evaluated at x    
+    def calc_right(x, xp, fp):
+        assert x[-1] > xp[-1]
+        dx = x[-1] - xp[-1]
+        s = (fp[-1] - fp[-2]) / (xp[-1] - xp[-2])
+        return fp[-1] + dx * s
+    
+    return np.interp(x, xp, fp, right=calc_right(x, xp, fp))
+
+def main():
+    datadir = "/data92/Quark/ctrl_2000d/HSt42_20"
+    ds = Dataset(datadir)
+    p_ref = np.linspace(2000, 100000, 20)
+
+    for iday in range(1000, 2000, 25):
+        f = ds.open_HDF5(iday)
+        # (time, lev, lat, lon)
+        # (200, 20, 64, 128)
+        p = f['grid_p_full_xyzt'][:]
+        t = f['grid_t_c_xyzt'][:]
+        qv = f['grid_tracers_c_xyzt'][:]
+        
+        res = (
+            interp_to_ref(p_ref, p[0, :, 32, 0], t[0, :, 32, 0]),
+            interp_to_ref(p_ref, p[0, :, 32, 0], qv[0, :, 32, 0])
+        )
+        
+        # fig, axs = plt.subplots(1, 2, figsize=(10, 10), sharey=True)
+        # axs[0].plot(res[0][15:], p_ref[15:], '.-', label='interp')
+        # axs[0].plot(t[0, 15:, 32, 0], p[0, 15:, 32, 0], '.-', label='raw')
+        # axs[1].plot(res[1][15:], p_ref[15:], label='interp')
+        # axs[1].invert_yaxis()
+                            
+
     
 # ==================================================
 from time import perf_counter
